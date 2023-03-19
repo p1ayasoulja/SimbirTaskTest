d0a3b734ea9ef8aac273dc10a322108220cb41d6
Andrey <andreyjeswork@gmail.com>
2023-03-19 21:37:27 +0300

    Added : some tests

--- src/main/java/com/example/mytaskmanagersimbirsofr/MyTaskManagerSimbirSoftApplication.java
+++ src/main/java/com/example/mytaskmanagersimbirsofr/MyTaskManagerSimbirSoftApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyTaskManagerSimbirSofrApplication {
+public class MyTaskManagerSimbirSoftApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyTaskManagerSimbirSofrApplication.class, args);
+        SpringApplication.run(MyTaskManagerSimbirSoftApplication.class, args);
     }
 
 }
---
+++ src/test/java/com/example/mytaskmanagersimbirsofr/MyTaskManagerSimbirSoftApplicationTests.java
@@ -0,0 +1,47 @@
+package com.example.mytaskmanagersimbirsofr;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MyTaskManagerSimbirSoftApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Good Morning, Mister!")));
+    }
+
+    @Test
+    public void accessTest() throws Exception {
+        this.mockMvc.perform(get("/dashboard"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+    @Test
+    public void badCredentials() throws Exception {
+        this.mockMvc.perform(post("/login").param("username", "1"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+}
+
+
